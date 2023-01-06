6799cc552f55df8a29a1f1af915aa4ebec3567ce
draco6417 <122049958+draco6417@users.noreply.github.com>
2023-01-06 23:49:13 +0530

    Add files via upload

---
+++ DisplayElement.java
@@ -0,0 +1,6 @@
+package weatherProject;
+
+public interface DisplayElement {
+	 public void display();
+
+}
---
+++ Observer.java
@@ -0,0 +1,6 @@
+package weatherProject;
+
+public interface Observer {
+	 public void update(float temp, float humidity, float pressure);
+
+}
